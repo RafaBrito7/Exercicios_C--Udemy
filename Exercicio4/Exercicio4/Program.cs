244ae3896a371a4b42372ea68a20a3a2a2b9c369
RafaBrito7 <rafabrito7@gmail.com>
2020-07-23 15:09:21 -0300

    Exercicio 4

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o ID do Funcionário: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o número de horas trabalhadas: ");
+            int workedHours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor que recebe por hora: ");
+            double paymentPerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double payment = workedHours * paymentPerHour;
+
+            Console.WriteLine("NUMBER = " + id);
+            Console.WriteLine("SALARY = " + (payment).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
