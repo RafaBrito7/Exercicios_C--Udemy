0b48a71c45c1693b28ee09515cbe59cae2059c62
RafaBrito7 <rafabrito7@gmail.com>
2020-07-23 14:17:28 -0300

    Adicionado Exercicio 1

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o número 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o número 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("SOMA = " + (num1 + num2));
+
+        }
+    }
+}
