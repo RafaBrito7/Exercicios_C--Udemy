11d7dd70e842cd1b30e826455e8d140d11f8b31b
RafaBrito7 <rafabrito7@gmail.com>
2020-07-23 14:54:24 -0300

    Excercicio 3

---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor de A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de C: ");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de D: ");
+            int d = int.Parse(Console.ReadLine());
+
+            int diferenca = a * b - c * d;
+
+            Console.WriteLine("DIFERENÇA = " + diferenca);
+        }
+    }
+}
