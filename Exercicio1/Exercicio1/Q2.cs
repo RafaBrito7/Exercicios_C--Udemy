5d7ff5473852db17eb79ccbd0ece28223a520cef
RafaBrito7 <rafabrito7@gmail.com>
2020-07-23 14:42:18 -0300

    Exercicio 2

---
+++ Exercicio1/Exercicio1/Q2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Exercicio1
+{
+    class Q2
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Informe o valor do Raio: ");
+            float raio = float.Parse(Console.ReadLine());
+
+            const float pi = 3.14159F;
+
+            Console.WriteLine("A = " + (pi * (raio * raio)));
+
+        }
+    }
+}
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor do Raio: ");
+            float raio = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            const float pi = 3.14159F;
+
+            Console.WriteLine("A = " + ((pi * (raio * raio))).ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
